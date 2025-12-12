b268060bad995637f63a00bcf2cd4fa8dcf8e967
nisharb911 <95846670+nisharb911@users.noreply.github.com>
2025-12-12 12:13:13 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,6 +22,16 @@ DATABASES = {
         }
     }
 }
+# for live deploye project
+#import dj_database_url
+
+#DATABASES = {
+#   'default': dj_database_url.config(
+#       default=os.environ.get("DATABASE_URL"),
+#       conn_max_age=600,
+#       ssl_require=True
+#   )
+#}
 
 
 INSTALLED_APPS = [
